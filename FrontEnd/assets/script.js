3b6c49b0e9f94fa4999eb9ff41aa9d884b7ecfa5
Thierry <thierry.pollastri@gmail.com>
2026-05-03 18:34:03 +0200

    Add dynamic category filter buttons
    Ajout du style des boutons de filtre dans style.css (conformément à la maquette Figma).
    Ajout du chargement dynamique des filtres via GET /categories.
    Extraction de createBtn en fonction réutilisable.
    Filtrage de la galerie au clic sans nouvel appel API.

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,12 +1,31 @@
 // Variables globales
+const filters = document.querySelector(".filters");
 const gallery = document.querySelector(".gallery");
-// let galleryWorkCount = 0;
 
 // Fonctions
-async function loadImages() {
+function createBtn(category, title) {
+  const newBtn = document.createElement("button");
+  newBtn.classList.add("filter-btn");
+  if (category === "all") {
+    newBtn.classList.add("active");
+  }
+  newBtn.dataset.category = category;
+  newBtn.textContent = title;
+  filters.appendChild(newBtn);
+}
+
+async function loadFilters() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const categories = await response.json();
+  createBtn("all", "Tous");
+  categories.forEach((category) => {
+    createBtn(category.id, category.name);
+  });
+}
+
+async function loadWorks() {
   const response = await fetch("http://localhost:5678/api/works");
   const works = await response.json();
-  // galleryWorkCount = works.length;
   works.forEach((work) => {
     const figure = document.createElement("figure");
     const image = document.createElement("img");
@@ -18,9 +37,41 @@ async function loadImages() {
 
     figure.appendChild(image);
     figure.appendChild(figcaption);
+    figure.dataset.category = work.categoryId;
+
     gallery.appendChild(figure);
   });
 }
 
+function selectFilter() {
+  if (event.target.tagName === "BUTTON") {
+    const btnActive = document.querySelector(".active");
+    const btnSelected = event.target; // la méthode .closest("button") n'est pas utile ici
+    if (btnActive != btnSelected) {
+      const allFigure = document.querySelectorAll("figure[data-category]");
+      btnActive.classList.remove("active");
+      btnSelected.classList.add("active");
+      const categorySelected = btnSelected.getAttribute("data-category");
+      // console.log("category : " + categorySelected);
+      allFigure.forEach((figure) => {
+        if (
+          figure.dataset.category == categorySelected ||
+          categorySelected == "all"
+        ) {
+          figure.style.display = "block";
+        } else {
+          figure.style.display = "none";
+        }
+      });
+    }
+  }
+}
+
 // Main
-loadImages();
+
+// __ Initialisation : async function init() inutile ici  les deux fonctions de chargement sont indépendantes l'une de l'autre
+loadFilters();
+loadWorks();
+
+// __ Event listeners
+filters.addEventListener("click", selectFilter); // ici selectFilter reçoit 'event' en premier paramètre
